ce9ba227b878f3f9b94c27a4a350fe7b4db40fbc
tryu <jimeng.jiang@gmail.com>
2020-04-22 13:41:34 -0400

    complete make_rec function

--- recommender_template.py
+++ recommender_template.py
@@ -26,10 +26,46 @@ class Recommender():
 
     def make_recs(self,):
         '''
-        given a user id or a movie that an individual likes
-        make recommendations
+        INPUT:
+        _id - either a user or movie id (int)
+        _id_type - "movie" or "user" (str)
+        train_data - dataframe of data as user-movie matrix
+        train_df - dataframe of training data reviews
+        movies - movies df
+        rec_num - number of recommendations to return (int)
+        user_mat - the U matrix of matrix factorization
+        movie_mat - the V matrix of matrix factorization
+        
+        OUTPUT:
+        recs - (array) a list or numpy array of recommended movies like the 
+                       given movie, or recs for a user_id given
         '''
-
+        val_users = train_data_df.index
+        rec_ids = create_ranked_df(movies, train_df)
+        
+        if _id_type == 'user':
+            if _id in train_data.index:
+                # Get the index of which row the user is in for use in U matrix
+                idx = np.where(val_users == _id)[0][0]
+                
+                # take the dot product of that row and the V matrix
+                preds = np.dot(user_mat[idx,:],movie_mat)
+                
+                # pull the top movies according to the prediction
+                indices = preds.argsort()[-rec_num:][::-1] #indices
+                rec_ids = train_data_df.columns[indices]
+                rec_names = get_movie_names(rec_ids)
+                
+            else:
+                # if we don't have this user, give just top ratings back
+                rec_names = popular_recommendations(_id, rec_num, ranked_movies)
+                
+        # Find similar movies if it is a movie that is passed
+        else:
+            rec_ids = find_similar_movies(_id)
+            rec_names = get_movie_names(rec_ids)
+    
+        return rec_ids, rec_names
 
 if __name__ == '__main__':
     # test different parts to make sure it works
